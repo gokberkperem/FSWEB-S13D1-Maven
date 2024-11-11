76894038b2066f131919ab7d7b0e08a1996fcf32
gokberkperem <peremgokberk@gmail.com>
2024-11-11 22:29:47 +0300

    11.11.24

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,9 +18,9 @@ public class Main {
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
         int lowestTemp = 25;
-        int highestTemp = isSummer ? 45:35;
+        int highestTemp = isSummer ? 45 : 35;
 
-        return temp < highestTemp && temp > lowestTemp;
+        return temp >= lowestTemp && temp <=highestTemp;
     }
 
     public static double area(double width, double height) {
